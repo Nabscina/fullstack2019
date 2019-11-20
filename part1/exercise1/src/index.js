cdbc40fee371950e57169a997ba3885c167f8fb5
Nabscina <bebenoctescere@gmail.com>
2019-11-20 19:33:41 +0200

    exercise 1.1 done

---
+++ part1/exercise1/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+    return (
+      <div>
+        <p>
+          {props.course}
+        </p>
+      </div>
+    )
+  }
+
+  const Content = (props) => {
+    return (
+      <div>
+        <p>
+          {props.part}, {props.exercises} exercises
+        </p>
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+      <div>
+        <p>
+          Total number of exercises {props.ex1 + props.ex2 + props.ex3}
+        </p>
+      </div>
+    )
+  }
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content exercises={exercises1} part={part1} />
+      <Content exercises={exercises2} part={part2} />
+      <Content exercises={exercises3} part={part3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))

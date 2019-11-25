58ff54898cdacb2d1170272a92dd7452b0319dea
Nabscina <bebenoctescere@gmail.com>
2019-11-25 15:21:23 +0200

    unicafe 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,28 +1,36 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  return (
-    <div>
-      <p>GIVE FEEDBACK</p>
-      <button onClick={() => setGood(good + 1)}> good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      <p>STATISTICS</p>
-      <div>{good} good</div>
-      <div>{neutral} neutral</div>
-      <div>{bad} bad</div>
-      <div>{good + bad + neutral} all</div>
-      <div>{(good - bad) / (good + bad + neutral)} average</div>
-      <div>{100 * good / (good + bad + neutral)} % positive</div>
-    </div>
-  )
+const Statistics = ({ good, bad, neutral }) => {
+    return (
+        <div>
+            <div>{good} good</div>
+            <div>{neutral} neutral</div>
+            <div>{bad} bad</div>
+            <div>{good + bad + neutral} all</div>
+            <div>{(good - bad) / (good + bad + neutral)} average</div>
+            <div>{100 * good / (good + bad + neutral)} % positive</div>
+        </div>
+    )
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    return (
+        <div>
+            <p>GIVE FEEDBACK</p>
+            <button onClick={() => setGood(good + 1)}> good</button>
+            <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+            <button onClick={() => setBad(bad + 1)}>bad</button>
+            <p>STATISTICS</p>
+            <Statistics good={good} bad={bad} neutral={neutral}/> 
+        </div>
+    )
+}
+
+ReactDOM.render(<App />,
+    document.getElementById('root')
 )

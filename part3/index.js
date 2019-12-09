2daa8dd0d2d1dd0752e5120e6864b2c94c745eba
Nabscina <bebenoctescere@gmail.com>
2019-12-09 20:03:19 +0200

    3.7 done

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
+app.use(morgan('tiny'))
 
 let persons = [
   {

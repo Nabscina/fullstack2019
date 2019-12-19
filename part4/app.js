7f2838a5f2c14e75d3e383119d694d67399347c6
Nabscina <bebenoctescere@gmail.com>
2019-12-19 19:42:35 +0200

    4.2 done

---
+++ part4/app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
+
--- part4/index.js
+++ part4/index.js
@@ -1,47 +1,9 @@
+const app = require('./app')
+const config = require('./utils/config')
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
 
+const server = http.createServer(app)
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const password = process.argv[2]
-
-const mongoUrl = 'mongodb+srv://fullstack:' + password + '@cluster0-zw1rt.mongodb.net/blog-app?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })

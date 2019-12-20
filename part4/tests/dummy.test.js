2f5da97c4b6f13d98ba76e5be46a2a1ed6a0a972
Nabscina <bebenoctescere@gmail.com>
2019-12-20 18:02:44 +0200

    4.4 done

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -6,3 +6,48 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('equals the likes of the only blog in the list', () => {
+    const result = listHelper.totalLikes([
+      {
+        title: 'Ketamiinioverit',
+        author: 'Nisti',
+        url: 'www.kek.de',
+        likes: 5
+      }
+    ])
+    expect(result).toBe(5)
+  })
+
+  test('of a bigger list is correct', () => {
+    const result = listHelper.totalLikes([
+      {
+        title: 'Ketamiinioverit',
+        author: 'Nisti',
+        url: 'www.kek.de',
+        likes: 5
+      },
+      {
+        title: 'The Eastern Sea is so Blue',
+        author: 'M. Gold',
+        url: 'www.biginjapan.com',
+        likes: 50
+      },
+      {
+        title: 'I Broke A Pencil In Half',
+        author: 'L. Tomlinson',
+        url: 'nocontr.ol',
+        likes: 125
+      }
+    ])
+    expect(result).toBe(180)
+  })
+
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+
+  let x = 0
+  blogs.map(blog => x += blog.likes)
+  return x
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
